422f113785ac17c80f8ddc9686d26c97f00e26ae
Smitty <smitop2@gmail.com>
2020-10-16 19:16:56 -0400

    better

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ mkdir harakaconf
 yarn run haraka -i harakaconf
 cp harakaplugins harakaconf/config/plugins
 cp harakasmtp.ini harakaconf/config/smtp.ini
+cp harakaquarantine.ini harakaconf/config/quarantine.ini
