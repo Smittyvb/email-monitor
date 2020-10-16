48c68cf49d2773f70f93e3111e950e217db6bf63
Smitty <smitop2@gmail.com>
2020-10-16 19:43:42 -0400

    get inbound working

--- init.sh
+++ init.sh
@@ -4,3 +4,4 @@ yarn run haraka -i harakaconf
 cp harakaplugins harakaconf/config/plugins
 cp harakasmtp.ini harakaconf/config/smtp.ini
 cp harakaquarantine.ini harakaconf/config/quarantine.ini
+cp harakahost_list_regex harakaconf/config/host_list_regex
